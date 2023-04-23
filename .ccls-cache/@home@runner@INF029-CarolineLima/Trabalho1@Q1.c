9ab28eb0cbae3ab6569a951d2181ab3b1a77f83b
CarolLimav <carol.lima.vitoria@gmail.com>
2023-04-23 19:01:41 +0000

    Trabalho1
    Finalizado.

---
+++ .ccls-cache/@home@runner@INF029-CarolineLima/Trabalho1@Q1.c
@@ -0,0 +1,31 @@
+#define TAMDATA 1
+
+struct dt{
+char dia [1];
+char mes[1];
+char ano [1];
+} Data; 
+
+Data datas [TAMDATA]; 
+
+
+int main(){
+
+
+
+
+
+  
+}
+
+void data(){
+printf("Digite a data no formato dd/mm/aaaa");
+printf("Dia:");
+  for(int i=0; i<=1; i++){
+scanf("%s", Data[i].dia); 
+  }
+
+
+
+  return 0; 
+}
